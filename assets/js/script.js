63e702be523bf2d888a1f32fb2002bba054d9abf
Gabriel Ruizz <garciagabriel1945@gmail.com>
2022-02-22 19:12:33 -0800

    fixed global vars and functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,24 +4,24 @@ const api = {
   five: "https://api.openweathermap.org/data/2.5/",
 };
 
-// //search form and button
-const searchInputEl = document.querySelector("#search");
-// const searchBtnEl = document.querySelector('#btn');
+var search = document.querySelector("#search");
+var btn = document.querySelector(".btn");
 
-// history and clear history button
-const historyEl = document.querySelector("#history");
-const clearBtnEl = document.querySelector("#clear");
+btn.addEventListener("click", getInput);
 
-const weatherIconEl = document.createElement("img");
-const currentCityEl = document.querySelector("#current-city");
-const currentTempEl = document.querySelector("current-temp");
-const currentHumidEl = document.querySelector("#current-humid");
-const currentWindEl = document.querySelector("#current-wind");
-const currentUvEl = document.querySelector("#current-uv");
+// current weather elements
+var currentCityEl = document.getElementById("current-city");
+var currentTempEl = document.getElementById("current-temp");
+var currentHumEl = document.getElementById("current-hum");
+var currentWindEl = document.getElementById("current-wind");
+var currentUvEl = document.getElementById("current-uv");
+// const historyEl = document.getElementById("history");
+// var clearBtn = document.getElementById("clear");
 
-// const fiveDayCast = document.querySelector("#five-day-cast");
+//
+var futureCast = document.getElementById("five-day-cast");
 
-let searchHistory = JSON.parse(localStorage.getItem("searchInputEl")) || [];
+let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
 function getInput() {
   getApi(search.value);
@@ -33,63 +33,61 @@ function getApi() {
     .then(function (response) {
       return response.json();
     })
-    .then(function (data) {
-      currentWeather(data);
+    .then(function (data1) {
+      updateWeather(data1);
     });
 }
 
-// appending data to current weather dom
-function currentWeather(data) {
-  console.log(data);
+// current weather function
+function updateWeather(data1) {
+  // console.log(data1);
 
   var img = document.createElement("img");
-  img.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+  img.src = "http://openweathermap.org/img/wn/" + data1.weather[0].icon + ".png";
 
   document.querySelector("#weather-img").appendChild(img);
-  currentTempEl.innerHTML = data.main.temp + " °";
+  currentTempEl.innerHTML = data1.main.temp + " °";
 
-  currentHumidEl.innerHTML = "Humidity: " + data.main.humidity + "%";
-  currentWindEl.innerHTML = data.wind.speed + " mph";
+  currentHumEl.innerHTML = "Humidity: " + data1.main.humidity + "%";
+  currentWindEl.innerHTML = data1.wind.speed + " mph";
 
-  var dateString = moment.unix(data.dt).format("MM/DD/YYYY");
+  var dateString = moment.unix(data1.dt).format("MM/DD/YYYY");
   console.log(dateString);
-  currentCityEl.innerHTML = data.name + " -" + " " + dateString;
+  currentCityEl.innerHTML = data1.name + " -" + " " + dateString;
 
   function getUV() {
-    let lat = data.coord.lat;
-    let lon = data.coord.lon;
-    console.log("LLLLLLL");
+    let lat = data1.coord.lat;
+    let lon = data1.coord.lon;
     console.log(lat);
     console.log(lon);
-    console.log("LLLLLLL");
     let uvURL =
       "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + api.key + "&cnt=1";
-    console.log(uvURL);
+    // console.log(uvURL);
 
     fetch(uvURL)
       .then(function (response) {
         return response.json();
       })
-      .then(function (data) {
-        currentUv(data);
+      .then(function (data3) {
+        updateUV(data3);
       });
   }
+
   getUV();
 
-  function currentUv(data) {
+  function updateUV(data3) {
     var uvIndex = document.createElement("p");
 
-    if (data[0].value < 5) {
+    if (data3[0].value < 5) {
       uvIndex.setAttribute("class", "badge badge-success");
-    } else if (data[0].value < 7) {
+    } else if (data3[0].value < 7) {
       uvIndex.setAttribute("class", "badge badge-warning");
     } else {
       uvIndex.setAttribute("class", "badge badge-danger");
     }
-    console.log(data[0].value);
-    uvIndex.innerHTML = data[0].value;
-    currentUvEl.innerHTML = "UV Index: ";
-    currentUvEl.append(uvIndex);
+    // console.log(data3[0].value);
+
+    currentUvEl.innerHTML = "UV Index: " + data3[0].value;
   }
 }
 
@@ -98,15 +96,15 @@ function getApiFive() {
     .then(function (response) {
       return response.json();
     })
-    .then(function (data) {
-      fiveDayCast(data);
+    .then(function (data2) {
+      updateFiveDay(data2);
     });
 }
 
-function fiveDayCast(data) {
-  console.log(data);
+function updateFiveDay(data2) {
+  console.log(data2);
   // converting UNIX timestamp into readable date
-  UNIX_timestamp = data.list[0].dt;
+  UNIX_timestamp = data2.list[0].dt;
   function timeConverter(UNIX_timestamp) {
     var a = new Date(UNIX_timestamp * 1000);
     var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
@@ -116,19 +114,20 @@ function fiveDayCast(data) {
     return time;
   }
 
-  var fiveDayEl = document.querySelector("#five-day-cast");
-  fiveDayEl.empty();
+  var fivedayEl = $("#five-day-cast");
+
+  fivedayEl.empty();
 
   for (var i = 0; i < 33; i += 8) {
-    console.log(data);
-    var dateString = moment.unix(data.list[i].dt).format("MM/DD/YYYY");
-    var temp = data.list[i].main.temp;
-    var city = data.city.name;
-    var humidity = data.list[i].main.humidity;
+    console.log(data2);
+    var dateString = moment.unix(data2.list[i].dt).format("MM/DD/YYYY");
+    var temp = data2.list[i].main.temp;
+    var city = data2.city.name;
+    var humidity = data2.list[i].main.humidity;
     var img = document.createElement("img");
     img.src = "http://openweathermap.org/img/wn/" + data2.list[i].weather[0].icon + ".png";
 
-    // futureCast = document.getElementById('five-day-cast');
+    futureCast = document.querySelector("#five-day-cast");
     cardContainer = document.createElement("div");
     cardBody = document.createElement("div");
     cardHeader = document.createElement("h5");
@@ -143,8 +142,7 @@ function fiveDayCast(data) {
     cardContainer.classList.add("col-2");
     cardContainer.classList.add("forecast");
 
-    // futureCast.append(cardContainer);
-    fiveDayEl.append(cardContainer);
+    futureCast.append(cardContainer);
     cardContainer.append(cardBody);
     cardBody.append(cardHeader);
     cardBody.append(cardTemp);
